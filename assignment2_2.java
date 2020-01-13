9e1213498a28082602360d5c9940beacc5925c00
Timon <19689305+timonvdvelde@users.noreply.github.com>
2020-01-13 11:48:00 +0100

    first

---
+++ assignment2_2.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    // Use this to generate random numbers, look at assignment 1.
+    double randomNumber = Math.random();
+    int randomDay;
+
+  }
+}
+
